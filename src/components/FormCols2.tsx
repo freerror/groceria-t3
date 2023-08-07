4c604c61d8bfbc2046b2cb74fdbac6939fad0d17
appuser <appuser@fastmail.com>
2023-08-08 06:20:18 +1200

    🦄 refactor

--- src/components/FormCols2.tsx
+++ src/components/FormCols2.tsx
@@ -1,32 +1,28 @@
 import { type ReactElement } from "react";
 
-function FormCols2(props: {
+interface FormCols2Props {
   panel?: ReactElement;
   leftPanel?: ReactElement;
   rightPanel?: ReactElement;
-}) {
+}
+
+function FormCols2({ panel, leftPanel, rightPanel }: FormCols2Props) {
   return (
     <div className="m-0 my-1 grid grid-cols-1 justify-center gap-3 sm:m-2 sm:grid-cols-2">
-      {props.panel ? (
-        <div className="card prose rounded-box col-span-2 h-[600px] max-w-none bg-neutral-100 p-5">
-          {props.panel}
+      {panel && (
+        <div className="card prose rounded-box col-span-2 h-[600px] max-w-none bg-neutral-100 p-5 shadow-md">
+          {panel}
         </div>
-      ) : (
-        ""
       )}
-      {props.leftPanel ? (
-        <div className="card prose rounded-box col-span-1 h-[600px] max-w-none bg-neutral-100 p-5">
-          {props.leftPanel}
+      {leftPanel && (
+        <div className="card prose rounded-box col-span-1 h-[600px] max-w-none bg-neutral-100 p-5 shadow-md">
+          {leftPanel}
         </div>
-      ) : (
-        ""
       )}
-      {props.rightPanel ? (
-        <div className="card prose rounded-box col-span-1 min-h-[600px] max-w-none bg-neutral-100 p-5">
-          {props.rightPanel}
+      {rightPanel && (
+        <div className="card prose rounded-box col-span-1 min-h-[600px] max-w-none bg-neutral-100 p-5 shadow-md">
+          {rightPanel}
         </div>
-      ) : (
-        ""
       )}
     </div>
   );
